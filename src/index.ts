ab2962208062bcb9f955e2d8ee60d682c908e70f
Radim Štěpaník <radim.stepanik@gmail.com>
2021-04-14 00:11:12 +0200

    ✨ Prepare new apis

--- src/index.ts
+++ src/index.ts
@@ -5,29 +5,41 @@ import { express } from './container/express';
 import { logger } from './container/system';
 import { getHystrixStream } from './services/hystrix-metrics';
 
-export const listen = () => {
-    const createHystrixDashboard = (portNumber = 8081): void => {
-        const dashboardSettings = {
-            idleTimeout: 4000,
-            interval: 2000,
-            proxy: true,
-        };
-
-        const router = expressApp
-            .Router()
-            .get('/', (req, res) => res.json({ dashboard: '/dashboard', stream: '/stream' }))
-            .use('/dashboard', dashboard(dashboardSettings))
-            .use('/stream', (req, res, _) => {
-                res.setHeader('Content-Type', 'text/event-stream;charset=UTF-8');
-                res.setHeader('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate');
-                res.setHeader('Pragma', 'no-cache');
-                getHystrixStream().pipe(res);
-            });
-
-        const app = expressApp();
-        app.use(router).listen(portNumber, () => logger.info('[Express] Hystrix dashboard listening at %s', portNumber));
+const createHystrixDashboard = (portNumber = 8081): void => {
+    const dashboardSettings = {
+        idleTimeout: 4000,
+        interval: 2000,
+        proxy: true,
     };
+
+    const router = expressApp
+        .Router()
+        .get('/', (req, res) => res.json({ dashboard: '/dashboard', stream: '/stream' }))
+        .use('/dashboard', dashboard(dashboardSettings))
+        .use('/stream', (req, res, _) => {
+            res.setHeader('Content-Type', 'text/event-stream;charset=UTF-8');
+            res.setHeader('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate');
+            res.setHeader('Pragma', 'no-cache');
+            getHystrixStream().pipe(res);
+        });
+
+    const app = expressApp();
+    app.use(router).listen(portNumber, () => logger.info('[Express] Hystrix dashboard listening at %s', portNumber));
+};
+
+const createSimpleService = (portNumber = 8082) => {
+    const router = expressApp.Router().use('/', async (req, res, _) => {
+        res.json({ iAmListeningOnPort: portNumber });
+    });
+
+    const app = expressApp();
+    app.use(router).listen(portNumber, () => logger.info('[Express] Simple service dashboard listening at %s', portNumber));
+};
+
+export const listen = () => {
     createHystrixDashboard();
+    createSimpleService();
+    createSimpleService(8083);
     return express
         .listen(config.port, () => {
             logger.info('[Express] Listening at %s', config.port);
